782fb6b0ff6d7885ae549cb78916881ac506ac81
Marcus Pointner <marcus.pointner@gmail.com>
2014-07-15 22:17:31 +0300

    initial commit

---
+++ src/Main.hx
@@ -0,0 +1,42 @@
+package;
+
+import flash.display.StageAlign;
+import flash.display.StageScaleMode;
+import flash.Lib;
+import ru.stablex.ui.UIBuilder;
+
+
+/**
+* Simple demo project for StablexUI
+*/
+class Main extends ru.stablex.ui.widgets.Widget{
+    /**
+    * Enrty point
+    *
+    */
+    static public function main () : Void{
+
+        Lib.current.stage.align     = StageAlign.TOP_LEFT;
+        Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+
+        //register Main so we can use it in xml.
+        UIBuilder.regClass("Main");
+
+        //initialize StablexUI
+        UIBuilder.init();
+
+        //Create our UI
+        Lib.current.addChild( 
+		
+		UIBuilder.buildFn(
+		
+			//	'/Users/marcus/Dropbox/DEV/haXe3/pm_haxe3_projects/UIstablex_scrolling/assets/ui/index.xml'
+			'assets/ui/index.xml'		
+		
+							)() );
+    }//function main()
+
+
+}//class Main
+
+
